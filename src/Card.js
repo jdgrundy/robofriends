d26cd70991184d729a2af5718c38a46e8591e9a2
System Administrator <root@Josephs-MacBook-Pro.local>
2024-07-19 23:51:16 +0100

    Completed RoboFriends React App

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ id, name, email }) => {
 	return (
-		<div className="bg-light-green dib br3 pa2 ma3 grow tc bw2 shadow-5">
+		<div className="bg-light-green dib br4 pa2 ma3 grow tc bw2 shadow-4 ">
 			<img src={`https://robohash.org/${id}?150x150`} alt="Profile" />
 			<div>
 				<h2>{name}</h2>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
 	return (
 		<div>
 			<input
-				className="pa2 ba b--green bg-lightest-blue"
+				className="pa3 br-pill w-30 ma1 ba b--green bg-lightest-blue"
 				type="search"
-				placeholder="Search your Robofriends"
+				placeholder="Search for your RoboFriends..."
+				onChange={searchChange}
 			/>
 		</div>
 	);
